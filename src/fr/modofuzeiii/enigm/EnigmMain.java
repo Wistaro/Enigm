2cf208f52501eabbda6cf02a6db419b28bd8fdd2
KingL <lorisabidule@free.fr>
2020-07-25 17:20:17 +0200

    OnEnableOnDisable

--- src/fr/modofuzeiii/enigm/EnigmMain.java
+++ src/fr/modofuzeiii/enigm/EnigmMain.java
@@ -5,17 +5,24 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class EnigmMain extends JavaPlugin {
 	@Override
 	public void onEnable() {
-
-		/*SALUT ENFANT DE LA LUNE*/
-
-		System.out.println("hello King!");
+		System.out.println("*******************");
+		System.out.println("*                 *");
+		System.out.println("*                 *");
+		System.out.println("*  Le plugin est  *");
+		System.out.println("*    En marche    *");
+		System.out.println("*                 *");
+		System.out.println("*                 *");
+		System.out.println("*******************");
 	}
-        @Override
-        public void onDisable() {
-                System.out.println("HELLO LE MONDE");
-
-                System.out.println("bye Wistaro!");
-				System.out.println("ceci est un testss");
-
+    @Override
+    public void onDisable() {
+        System.out.println("*******************");
+    	System.out.println("*                 *");
+    	System.out.println("*                 *");
+    	System.out.println("*  Le plugin est  *");
+    	System.out.println("*      Arrêté     *");
+    	System.out.println("*                 *");
+    	System.out.println("*                 *");
+    	System.out.println("*******************");
         }
 }
