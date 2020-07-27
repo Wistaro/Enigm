32f531ee3afe0203e5f26d31f6fbc7c950f73b60
Wistaro <wistaro@hotmail.fr>
2020-07-27 20:24:06 +0200

    Improve compass GUI

--- src/fr/modofuzeiii/enigm/AdminEvents.java
+++ src/fr/modofuzeiii/enigm/AdminEvents.java
@@ -1,5 +1,7 @@
 package fr.modofuzeiii.enigm;
 
+import java.util.ArrayList;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -7,7 +9,11 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import fr.modofuzeiii.enigm.gui.AdminCompassGui;
 
 
 public class AdminEvents implements Listener {
@@ -18,11 +24,37 @@ public class AdminEvents implements Listener {
 		Player p = ev.getPlayer();
 		ItemStack clickItem = p.getInventory().getItemInMainHand();
 		
+		
 		//Todo : - vérifier les perms du joueur
 		//       - Si le gars a 2 compass ça marche po
 		
 		if(clickItem.equals(new ItemStack(Material.COMPASS))) {
-			//open GUI  - 
+			
+			p.sendMessage("Tu as cliqué sur la bousolle!");
+			
+			AdminCompassGui guiCompass = new AdminCompassGui(p); //open the compass GUI
+			
+			 
+			
+			 /* Inventory help = Bukkit.getServer().createInventory(p, 9, "Help");
+			  
+			  ItemStack ref1 = new ItemStack(Material.BOOK);
+              ItemMeta metaref1 = ref1.getItemMeta();
+              ArrayList<String> lore= new ArrayList<String>();
+
+              lore.add(" ");
+              lore.add("§for visit our site");
+              lore.add(" ");
+              lore.add("§atest.net/help");
+              
+              metaref1.setLore(lore);
+              metaref1.setDisplayName("§6§lClick to get help");
+      
+
+              ref1.setItemMeta(metaref1);
+  
+          //Here opens the inventory
+          p.openInventory(help);*/
 		}
 	}
 
