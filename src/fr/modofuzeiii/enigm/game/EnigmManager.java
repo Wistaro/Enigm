9c0cf856f345b2ae2df2bb311e66172976785bc1
KingL <lorisabidule@free.fr>
2020-07-28 12:22:48 +0200

    dd

--- src/fr/modofuzeiii/enigm/game/EnigmManager.java
+++ src/fr/modofuzeiii/enigm/game/EnigmManager.java
@@ -3,11 +3,19 @@ package fr.modofuzeiii.enigm.game;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class EnigmManager implements CommandExecutor {
 
 	@Override
-	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
+	public boolean onCommand(CommandSender sender, Command cmd, String msg, String[] args) {
+		
+		if(sender instanceof Player) {
+			Player p = (Player)sender;
+			
+			
+		
+		}
 		
 		return false;
 	}
